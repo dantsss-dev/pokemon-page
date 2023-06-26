69b97e43107d09ffde751bfee25aba7149f79ec3
dantsss-dev <danieliseopc@gmail.com>
2023-06-25 21:47:52 -0600

    :bug: Modal bug fixed

--- src/components/PokeCard/SkeletonCard.js
+++ src/components/PokeCard/SkeletonCard.js
@@ -1,13 +1,12 @@
 export const SkeletonCard = () => {
-	const unused = '';
-	return (
-		<div className="w-full sm:w-1/3 border border-gray-300 shadow rounded-md p-4 mt-8 max-w-sm w-full mx-auto">
-			<div className="animate-pulse flex flex-wrap justify-center space-x-4">
-				<div className="mb-8 rounded-full bg-gray-400 h-24 w-24"></div>
-				<div className="w-full space-y-4 py-1">
-					<div className="h-4 bg-gray-400 rounded w-3/4"></div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full sm:w-1/3 border border-gray-300 shadow rounded-md p-4 mt-8 max-w-sm w-full mx-auto">
+      <div className="animate-pulse flex flex-wrap justify-center space-x-4">
+        <div className="mb-8 rounded-full bg-gray-400 h-24 w-24"></div>
+        <div className="w-full space-y-4 py-1">
+          <div className="h-4 bg-gray-400 rounded w-3/4"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
