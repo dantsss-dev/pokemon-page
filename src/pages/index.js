f8c299679ec1845555c76f60e6df3e0621dfa2eb
Walter Macall <macallwalter@gmail.com>
2023-06-26 00:59:07 -0600

    🚧 Add prettier config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
-import PokemonList from '../components/PokemonList';
+import HeadData from '@/components/HeadData'
 
-export default function Home() {
-	return (
-		<div className="container">
-			<main>
-				<PokemonList />
-			</main>
-		</div>
-	);
+const Home = () => {
+  return (
+    <div className="container">
+      <HeadData />
+      <main className="container mx-auto px-4">
+        <div className="flex flex-col justify-center items-center h-screen">
+          <h1 className="text-4xl font-bold text-white">Pokemon App</h1>
+        </div>
+      </main>
+    </div>
+  )
 }
+
+export default Home
